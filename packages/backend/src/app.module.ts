2158a9f904cc003f3436619f356bb7d7997d6d18
CKMONISH2000 <ckmonish2000@gmail.com>
2024-07-28 02:52:02 +0530

    feat: added templates module (#5)
    * chore: scaffolded templates module
    
    * feat: added project templates api
    
    ---------
    
    Co-authored-by: ckmonish2000 <>

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -4,8 +4,9 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ProjectsModule } from './projects/projects.module';
 import { ComponentsModule } from './components/components.module';
+import { TemplatesModule } from './templates/templates.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule,ProjectsModule, ComponentsModule],
+  imports: [PrismaModule, AuthModule, UserModule,ProjectsModule, ComponentsModule, TemplatesModule],
 })
 export class AppModule {}
