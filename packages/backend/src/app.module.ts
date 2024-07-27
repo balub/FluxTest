1377b3d2669b76d121b043195f624beaf4a20593
ckmonish2000 <>
2024-07-28 01:42:13 +0530

    chore: defined schemas

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ProjectsModule } from './projects/projects.module';
+import { ComponentsModule } from './components/components.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule],
+  imports: [PrismaModule, AuthModule, UserModule,ProjectsModule, ComponentsModule],
 })
 export class AppModule {}
