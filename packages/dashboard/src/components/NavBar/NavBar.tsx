6f238ce25103eac29e9362c33c5809a1e20294f5
aaronsww <jeevan.aaron.joseph@gmail.com>
2024-07-28 05:39:41 +0530

    refactor: home nav

--- packages/dashboard/src/components/NavBar/NavBar.tsx
+++ packages/dashboard/src/components/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
@@ -42,8 +42,9 @@ export default function NavBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={()=> {navigate('/')}}
           >
-            <MenuIcon />
+             <HomeIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             FluxTest
